1471af716e3bfd6b8b5d5d7e9cd87ed8ba49f71f
Walker Henderson <wjahenderson@gmail.com>
2016-03-03 01:28:18 -0800

    Added the run_if feature

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate time;
 
 use rand::{thread_rng, Rng};
 use time::{precise_time_ns};
+use std::marker::PhantomData;
 
 #[derive(Debug)]
 pub struct CohortResult<T: Clone> {
@@ -37,13 +38,30 @@ impl<CurrentT: Clone, NewT: Clone> ExperimentResult<CurrentT, NewT> {
     }
 }
 
-pub trait Experiment<CurrentResult: Clone, NewResult: Clone> {
-    fn publish(_: ExperimentResult<CurrentResult, NewResult>) {}
+pub struct ExperimentBuilder<'a, CurrentResult: Clone, NewResult: Clone, Param: Clone, E: Experiment<CurrentResult, NewResult, Param> + ?Sized> {
+    name: &'static str,
+    current: Box<FnMut(Param) -> CurrentResult + 'a>,
+    new: Box<FnMut(Param) -> NewResult + 'a>,
+    run_if: Option<Box<FnMut(Param) -> bool + 'a>>,
+    experiment: PhantomData<E>
+}
 
-    fn carry_out<C, N, Param>(name: &'static str, mut current: C, mut new: N, param: Param) -> CurrentResult
-        where C: FnMut(Param) -> CurrentResult,
-              N: FnMut(Param) -> NewResult,
-              Param: Clone {
+impl<'a, CurrentResult: Clone, NewResult: Clone, Param: Clone, E: Experiment<CurrentResult, NewResult, Param>> ExperimentBuilder<'a, CurrentResult, NewResult, Param, E> {
+    pub fn run_if<R>(mut self, run_if: R) -> Self
+            where R: FnMut(Param) -> bool + 'a {
+        self.run_if = Some(Box::new(run_if));
+        self
+    }
+
+    pub fn carry_out(mut self, param: Param) -> CurrentResult {
+        match self.run_if {
+            Some(mut r) => {
+                if !r(param.clone()) {
+                    return (self.current)(param.clone());
+                }
+            }
+            _ => {}
+        }
         let mut rng = thread_rng();
         let mut did_one = false;
         let current_goes_first: bool = rng.gen();
@@ -54,7 +72,7 @@ pub trait Experiment<CurrentResult: Clone, NewResult: Clone> {
         loop {
             if (current_goes_first || did_one) && !(current_goes_first && did_one) {
                 let start = precise_time_ns();
-                current_val = Some(current(param.clone()));
+                current_val = Some((self.current)(param.clone()));
                 current_duration = precise_time_ns() - start;
                 if did_one {
                     break;
@@ -64,7 +82,7 @@ pub trait Experiment<CurrentResult: Clone, NewResult: Clone> {
             }
             if (!current_goes_first || did_one) && !(!current_goes_first && did_one) {
                 let start = precise_time_ns();
-                new_val = Some(new(param.clone()));
+                new_val = Some((self.new)(param.clone()));
                 new_duration = precise_time_ns() - start;
                 if did_one {
                     break;
@@ -73,12 +91,29 @@ pub trait Experiment<CurrentResult: Clone, NewResult: Clone> {
                 }
             }
         }
-        Self::publish(ExperimentResult::new(
+        E::publish(ExperimentResult::new(
             CohortResult::new(current_duration as f64 * 1e-9, &current_val.as_ref().unwrap()),
             CohortResult::new(new_duration as f64 * 1e-9, &new_val.as_ref().unwrap()),
-            name
+            self.name
         ));
         current_val.unwrap()
+    }
+}
+
+
+pub trait Experiment<CurrentResult: Clone, NewResult: Clone, Param: Clone> {
+    fn publish(_: ExperimentResult<CurrentResult, NewResult>) {}
+
+    fn new<'a, C, N>(name: &'static str, current: C, new: N) -> ExperimentBuilder<'a, CurrentResult, NewResult, Param, Self>
+        where C: FnMut(Param) -> CurrentResult + 'a,
+              N: FnMut(Param) -> NewResult + 'a {
+        ExperimentBuilder {
+            name: name,
+            current: Box::new(current),
+            new: Box::new(new),
+            run_if: None,
+            experiment: PhantomData
+        }
     }
 }
 
@@ -87,33 +122,26 @@ mod test {
     use super::{Experiment, ExperimentResult};
     struct TestExperiment;
 
-    impl Experiment<String, String> for TestExperiment {
+    impl Experiment<String, String, ()> for TestExperiment {
         fn publish(result: ExperimentResult<String, String>) {
-            println!("{:?}", result);
+            println!("{:#?}", result);
         }
     }
 
     #[test]
     fn it_works() {
-        let a_str = "bagelman";
-        let a = TestExperiment::carry_out(
-            "experiment!",
+        let a_str = vec!['a', 'b', 'c'];
+        let a = TestExperiment::new("experiment!",
             |_| {
                 println!("current went!");
-                a_str.to_string()
+                a_str.clone().into_iter().collect()
             },
             |_| {
                 println!("new went!");
-                a_str.to_string()
-            },
-            ()
-        );
+                a_str.clone().into_iter().collect()
+            })
+            .run_if(|_| true)
+            .carry_out(());
         println!("{}", a);
     }
-}
-
-#[doc(hidden)]
-pub mod internal {
-    pub use rand;
-    pub use time;
 }
