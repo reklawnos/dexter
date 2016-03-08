e4167b79c1819ecad0c6baea4fde6d430432dd99
Walker Henderson <wjahenderson@gmail.com>
2016-03-08 00:06:11 -0800

    Cleaned some more things up and added ignore_if

--- src/lib.rs
+++ src/lib.rs
@@ -22,54 +22,71 @@ impl<T: Clone> CohortResult<T> {
 }
 
 #[derive(Debug)]
-pub struct ExperimentResult<CurrentT: Clone, NewT: Clone> {
-    pub current: CohortResult<CurrentT>,
-    pub new: CohortResult<NewT>,
-    pub name: &'static str
+pub struct ExperimentResult<Cr: Clone, Nr: Clone> {
+    pub name: &'static str,
+    pub current: CohortResult<Cr>,
+    pub new: CohortResult<Nr>,
+    pub match_type: MatchType
 }
 
-impl<CurrentT: Clone, NewT: Clone> ExperimentResult<CurrentT, NewT> {
-    fn new(current: CohortResult<CurrentT>, new: CohortResult<NewT>, name: &'static str) -> Self {
+#[derive(Debug)]
+pub enum MatchType {
+    Match,
+    NoMatch,
+    Ignored
+}
+
+impl<Cr: Clone, Nr: Clone> ExperimentResult<Cr, Nr> {
+    fn new(name: &'static str, current: CohortResult<Cr>, new: CohortResult<Nr>, match_type: MatchType) -> Self {
         ExperimentResult {
+            name: name,
             current: current,
             new: new,
-            name: name
+            match_type: match_type
         }
     }
 }
 
-pub struct ExperimentBuilder<'a, Cr: Clone, Nr: Clone, P, E: Experiment<Cr, Nr, P> + ?Sized> {
+pub struct ExperimentBuilder<'a, P, Cr: Clone, Nr: Clone, E: Experiment<P, Cr, Nr> + ?Sized> {
     name: &'static str,
     current: Box<FnMut(&P) -> Cr + 'a>,
     new: Box<FnMut(&P) -> Nr + 'a>,
     setup: Option<Box<FnMut(P) -> P + 'a>>,
     run_if: Option<Box<FnMut(&P) -> bool + 'a>>,
+    ignore_if: Option<Box<FnMut(&P) -> bool + 'a>>,
     experiment: PhantomData<E>
 }
 
-impl<'a, Cr: Clone, Nr: Clone, P, E: Experiment<Cr, Nr, P>> ExperimentBuilder<'a, Cr, Nr, P, E> {
+impl<'a, P, Cr: Clone, Nr: Clone, E: Experiment<P, Cr, Nr>> ExperimentBuilder<'a, P, Cr, Nr, E> {
     pub fn setup<S>(mut self, setup: S) -> Self
             where S: FnMut(P) -> P + 'a {
         self.setup = Some(Box::new(setup));
         self
     }
+
     pub fn run_if<R>(mut self, run_if: R) -> Self
             where R: FnMut(&P) -> bool + 'a {
         self.run_if = Some(Box::new(run_if));
         self
     }
 
+    pub fn ignore_if<I>(mut self, ignore_if: I) -> Self
+            where I: FnMut(&P) -> bool + 'a {
+        self.ignore_if = Some(Box::new(ignore_if));
+        self
+    }
+
     pub fn carry_out(mut self, mut param: P) -> Cr {
-        if let Some(mut s) = self.setup {
-            param = s(param);
+        if !E::enabled() {
+            return (self.current)(&param);
         }
-        match self.run_if {
-            Some(mut r) => {
-                if !r(&param) {
-                    return (self.current)(&param);
-                }
+        if let Some(mut setup) = self.setup {
+            param = setup(param);
+        }
+        if let Some(mut run_if) = self.run_if {
+            if !run_if(&param) {
+                return (self.current)(&param);
             }
-            _ => {}
         }
 
         let mut rng = thread_rng();
@@ -93,20 +110,39 @@ impl<'a, Cr: Clone, Nr: Clone, P, E: Experiment<Cr, Nr, P>> ExperimentBuilder<'a
                 }
             }
         }
+
+        let ignore = if let Some(mut ignore_if) = self.ignore_if {
+            ignore_if(&param)
+        } else {
+            false
+        };
+
         E::publish(ExperimentResult::new(
+            self.name,
             CohortResult::new(current_duration as f64 * 1e-9, &current_val.as_ref().unwrap()),
             CohortResult::new(new_duration as f64 * 1e-9, &new_val.as_ref().unwrap()),
-            self.name
+            if ignore {
+                MatchType::Ignored
+            } else if E::compare(&current_val.as_ref().unwrap(), &new_val.as_ref().unwrap()) {
+                MatchType::Match
+            } else {
+                MatchType::NoMatch
+            }
         ));
+
         current_val.unwrap()
     }
 }
 
 
-pub trait Experiment<Cr: Clone, Nr: Clone, P> {
+pub trait Experiment<P, Cr: Clone, Nr: Clone> {
     fn publish(_: ExperimentResult<Cr, Nr>) {}
 
-    fn new<'a, C, N>(name: &'static str, current: C, new: N) -> ExperimentBuilder<'a, Cr, Nr, P, Self>
+    fn enabled() -> bool {
+        true
+    }
+
+    fn new<'a, C, N>(name: &'static str, current: C, new: N) -> ExperimentBuilder<'a, P, Cr, Nr, Self>
         where C: FnMut(&P) -> Cr + 'a,
               N: FnMut(&P) -> Nr + 'a {
         ExperimentBuilder {
@@ -115,9 +151,12 @@ pub trait Experiment<Cr: Clone, Nr: Clone, P> {
             new: Box::new(new),
             setup: None,
             run_if: None,
+            ignore_if: None,
             experiment: PhantomData
         }
     }
+
+    fn compare(current_result: &Cr, new_result: &Nr) -> bool;
 }
 
 #[cfg(test)]
@@ -125,9 +164,13 @@ mod test {
     use super::{Experiment, ExperimentResult};
     struct TestExperiment;
 
-    impl Experiment<String, String, Vec<char>> for TestExperiment {
+    impl Experiment<Vec<char>, String, String> for TestExperiment {
         fn publish(result: ExperimentResult<String, String>) {
             println!("{:#?}", result);
+        }
+
+        fn compare(current_result: &String, new_result: &String) -> bool {
+            current_result == new_result
         }
     }
 
@@ -141,10 +184,13 @@ mod test {
             },
             |p| {
                 println!("new went!");
-                p.clone().into_iter().collect()
+                let mut p = p.clone();
+                p.reverse();
+                p.into_iter().collect()
             })
             .setup(|mut p| { p.sort(); p })
             .run_if(|_| true)
+            .ignore_if(|_| false)
             .carry_out(a_str);
         println!("{}", a);
     }
