eb6abbd050b26d831c7e29d193cbfbcda0ff2b97
Walker Henderson <wjahenderson@gmail.com>
2016-03-09 01:54:03 -0800

    First crack at documentation, changed naming of result struct, added licesnse file

--- readme.md
+++ readme.md
@@ -1,3 +1,34 @@
 Dexter
 ======
 A cool thing that does stuff like [Scientist](https://github.com/github/scientist), but for Rust!
+
+Here's an example:
+
+```rust
+struct ExamplePublisher;
+
+impl Publisher<Vec<char>, String, String> for ExamplePublisher {
+    fn publish(&mut self, result: ExperimentResult<String, String>) {
+        println!("{:#?}", result);
+    }
+
+    fn compare(&mut self, current_result: &String, new_result: &String) -> bool {
+        current_result == new_result
+    }
+}
+
+fn main() {
+  let chars = vec!['a', 'b', 'c'];
+  let mut p = TestPublisher{ result: None };
+  let result = Experiment::new("experiment",
+      |a: &Vec<char>| {
+          a.clone().into_iter().collect()
+      },
+      |a: &Vec<char>| {
+          a.clone().into_iter().collect()
+      })
+      .run_if(|p| { p.len() == 3 })
+      .carry_out(chars.clone(), &mut p);
+  println!("{}", result);
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,63 @@
+//! A library for using the Branch by Abstraction pattern to test for performance and correctness.
+#![warn(missing_docs, missing_debug_implementations,
+        missing_copy_implementations, trivial_casts,
+        trivial_numeric_casts, unsafe_code,
+        unstable_features, unused_extern_crates,
+        unused_import_braces, unused_qualifications,
+        unused_results, variant_size_differences)]
 extern crate rand;
 extern crate time;
 
+use std::fmt;
 use rand::{thread_rng, Rng};
 use time::precise_time_ns;
 
+/// Result for a subject in an experiment.
 #[derive(Debug)]
-pub struct CohortResult<T: Clone> {
+pub struct SubjectResult<T: Clone> {
+    /// Time spent running this subject's code
     pub duration: f64,
+    /// The value produced by this subject's code
     pub result: T
 }
 
-impl<T: Clone> CohortResult<T> {
+impl<T: Clone> SubjectResult<T> {
     fn new(duration: f64, result: &T) -> Self {
-        CohortResult {
+        SubjectResult {
             duration: duration,
             result: result.clone()
         }
     }
 }
 
+/// Result of an experiment.
 #[derive(Debug)]
 pub struct ExperimentResult<Cr: Clone, Nr: Clone> {
+    /// Name of the experiment
     pub name: &'static str,
-    pub current: CohortResult<Cr>,
-    pub new: CohortResult<Nr>,
+    /// Result for the current subject
+    pub current: SubjectResult<Cr>,
+    /// Result for the new subject
+    pub new: SubjectResult<Nr>,
+    /// The match type for this experiment
     pub match_type: MatchType
 }
 
+
+
+/// Matching type for experiments.
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum MatchType {
+    /// The two subjects had matching results
     Match,
+    /// The two subjects did not have matching results
     NoMatch,
+    /// The matching state was ignored
     Ignored
 }
 
 impl<Cr: Clone, Nr: Clone> ExperimentResult<Cr, Nr> {
-    fn new(name: &'static str, current: CohortResult<Cr>, new: CohortResult<Nr>, match_type: MatchType) -> Self {
+    fn new(name: &'static str, current: SubjectResult<Cr>, new: SubjectResult<Nr>, match_type: MatchType) -> Self {
         ExperimentResult {
             name: name,
             current: current,
@@ -45,6 +67,8 @@ impl<Cr: Clone, Nr: Clone> ExperimentResult<Cr, Nr> {
     }
 }
 
+/// A struct for building Dexter experiments.
+#[must_use]
 pub struct Experiment<'a, P, Cr: Clone, Nr: Clone> {
     name: &'static str,
     current: Box<FnMut(&P) -> Cr + 'a>,
@@ -55,37 +79,53 @@ pub struct Experiment<'a, P, Cr: Clone, Nr: Clone> {
 }
 
 impl<'a, P, Cr: Clone, Nr: Clone> Experiment<'a, P, Cr, Nr> {
-    pub fn new<C, N>(name: &'static str, current: C, new: N) -> Self
+    /// Constructs a new `Experiment` with a current and given subject.
+    pub fn new<C, N>(name: &'static str, current_subject: C, new_subject: N) -> Self
         where C: FnMut(&P) -> Cr + 'a,
               N: FnMut(&P) -> Nr + 'a {
         Experiment {
             name: name,
-            current: Box::new(current),
-            new: Box::new(new),
+            current: Box::new(current_subject),
+            new: Box::new(new_subject),
             setup: None,
             run_if: None,
             ignore_if: None
         }
     }
 
+    /// Adds a setup step to the experiment.
+    ///
+    /// The setup function can alter the parameter that's passed into the experiment before it is
+    /// passed to the `run_if` closure, the `ignore_if` closure, and the two subject closures.
     pub fn setup<S>(mut self, setup: S) -> Self
             where S: FnMut(P) -> P + 'a {
         self.setup = Some(Box::new(setup));
         self
     }
 
+    /// Adds a check step that will disable the experiment in certain cases.
+    ///
+    /// If the passed closure returns false when passed the experiment's parameter, then the
+    /// experiment will return the current subject's result without publishing.
     pub fn run_if<R>(mut self, run_if: R) -> Self
             where R: FnMut(&P) -> bool + 'a {
         self.run_if = Some(Box::new(run_if));
         self
     }
 
+    /// Adds an check step that will ignore mismatches in certain cases.
+    ///
+    /// If the passed closure returns true when passed the experiment's parameter, then the
+    /// result will have a `MatchType` of `Ignored`.
     pub fn ignore_if<I>(mut self, ignore_if: I) -> Self
             where I: FnMut(&P) -> bool + 'a {
         self.ignore_if = Some(Box::new(ignore_if));
         self
     }
 
+    /// Carry out the experiment given a parameter and a publisher.
+    ///
+    /// Returns the result of the current subject closure.
     pub fn carry_out<Pub: Publisher<P, Cr, Nr>>(mut self, mut param: P, publisher: &mut Pub) -> Cr {
         if !publisher.enabled() {
             return (self.current)(&param);
@@ -131,8 +171,8 @@ impl<'a, P, Cr: Clone, Nr: Clone> Experiment<'a, P, Cr, Nr> {
 
         publisher.publish(ExperimentResult::new(
             self.name,
-            CohortResult::new(current_duration as f64 * 1e-9, &current_val.as_ref().unwrap()),
-            CohortResult::new(new_duration as f64 * 1e-9, &new_val.as_ref().unwrap()),
+            SubjectResult::new(current_duration as f64 * 1e-9, &current_val.as_ref().unwrap()),
+            SubjectResult::new(new_duration as f64 * 1e-9, &new_val.as_ref().unwrap()),
             if ignore {
                 MatchType::Ignored
             } else if comparison {
@@ -146,13 +186,50 @@ impl<'a, P, Cr: Clone, Nr: Clone> Experiment<'a, P, Cr, Nr> {
     }
 }
 
+impl<'a, P, Cr: Clone, Nr: Clone> fmt::Debug for Experiment<'a, P, Cr, Nr> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let fn_str = "Fn(...)";
+        let some_fn_str = "Some(Fn(...))";
+        let none_str = "None";
+        f.debug_struct("Experiment")
+            .field("name", &self.name)
+            .field("current", &fn_str)
+            .field("new", &fn_str)
+            .field("setup", if self.setup.is_some() {
+                &some_fn_str
+            } else {
+                &none_str
+            })
+            .field("run_if", if self.run_if.is_some() {
+                &some_fn_str
+            } else {
+                &none_str
+            })
+            .field("ignore_if", if self.ignore_if.is_some() {
+                &some_fn_str
+            } else {
+                &none_str
+            })
+            .finish()
+    }
+}
 
+/// Trait for publishers, which are used by Dexter to store results of experiments.
 pub trait Publisher<P, Cr: Clone, Nr: Clone> {
-    fn publish(&mut self, _: ExperimentResult<Cr, Nr>) {}
+    /// Publish the result of an experiment.
+    fn publish(&mut self, result: ExperimentResult<Cr, Nr>);
 
+    /// Comparison function for the results of the subjects.
+    ///
+    /// This function should return `true` if the results "match," and `false` if they do not.
+    fn compare(&mut self, current_result: &Cr, new_result: &Nr) -> bool;
+
+    /// Only run the experiment in some cases.
+    ///
+    /// If `enabled` returns false, then the result of the current subject is used and no results
+    /// are published. This is meant to be an inexpensive function that gets called when every
+    /// experiment runs.
     fn enabled(&mut self) -> bool {
         true
     }
-
-    fn compare(&mut self, current_result: &Cr, new_result: &Nr) -> bool;
 }
